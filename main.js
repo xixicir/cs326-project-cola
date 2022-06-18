74e57ea1119c0c2922c925fb4cb0960fdc094ce4
Hongxi Huang <hongxihuang@umass.edu>
2022-06-18 14:22:35 -0400

    structure changed

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-document
+document.getElementById("fms").addEventListener("click", () => {
+
+});
+document.getElementById("f2s").addEventListener("click", () => {
+    
+});
+document.getElementById("f3s").addEventListener("click", () => {
+    
+});
+document.getElementById("fs").addEventListener("click", () => {
+    
+});
+document.getElementById("cfs").addEventListener("click", () => {
+    
+});
